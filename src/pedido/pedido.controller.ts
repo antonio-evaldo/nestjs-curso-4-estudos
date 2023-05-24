e5b801535a61b6c39d374c6259b7ecb3340ec913
camilapessoa <camilabentespessoa@gmail.com>
2023-05-24 17:01:12 -0300

    aula 2.4

--- src/pedido/pedido.controller.ts
+++ src/pedido/pedido.controller.ts
@@ -1,34 +1,19 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { PedidoService } from './pedido.service';
 import { CreatePedidoDto } from './dto/create-pedido.dto';
 import { UpdatePedidoDto } from './dto/update-pedido.dto';
 
-@Controller('pedido')
+@Controller('pedidos')
 export class PedidoController {
-  constructor(private readonly pedidoService: PedidoService) {}
+  constructor(private readonly pedidoService: PedidoService) { }
 
   @Post()
-  create(@Body() createPedidoDto: CreatePedidoDto) {
-    return this.pedidoService.create(createPedidoDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.pedidoService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.pedidoService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePedidoDto: UpdatePedidoDto) {
-    return this.pedidoService.update(+id, updatePedidoDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.pedidoService.remove(+id);
+  async criaPedido(
+    @Query('usuarioId') usuarioId: string,
+  ) {
+    const pedidoCriado = await this.pedidoService.cadastraPedido(
+      usuarioId
+    )
+    return pedidoCriado
   }
 }
--- src/pedido/pedido.service.ts
+++ src/pedido/pedido.service.ts
@@ -1,26 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePedidoDto } from './dto/create-pedido.dto';
 import { UpdatePedidoDto } from './dto/update-pedido.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PedidoEntity } from './pedido.entity';
+import { Repository } from 'typeorm';
+import { UsuarioEntity } from 'src/usuario/usuario.entity';
+import { StatusPedido } from './enum/statuspedido.enum';
 
 @Injectable()
 export class PedidoService {
-  create(createPedidoDto: CreatePedidoDto) {
-    return 'This action adds a new pedido';
+  constructor(
+    @InjectRepository(PedidoEntity)
+    private readonly pedidoRepository: Repository<PedidoEntity>,
+    @InjectRepository(UsuarioEntity)
+    private readonly usuarioRepository: Repository<UsuarioEntity>,
+  ) { }
+
+  async cadastraPedido(usuarioId: string) {
+    const usuario = await this.usuarioRepository.findOneBy({ id: usuarioId })
+    const pedidoEntity = new PedidoEntity();
+
+    pedidoEntity.valorTotal = 0;
+    pedidoEntity.status = StatusPedido.EM_PROCESSAMENTO
+    pedidoEntity.usuario = usuario
+
+    const pedidoCriado = await this.pedidoRepository.save(pedidoEntity)
+    return pedidoCriado
   }
 
-  findAll() {
-    return `This action returns all pedido`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} pedido`;
-  }
-
-  update(id: number, updatePedidoDto: UpdatePedidoDto) {
-    return `This action updates a #${id} pedido`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} pedido`;
-  }
 }
