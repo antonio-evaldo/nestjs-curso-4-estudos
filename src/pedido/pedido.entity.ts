61838a33aa3b5f79798f67e3ec3b7274181d8909
camilapessoa <camilabentespessoa@gmail.com>
2023-05-24 15:08:19 -0300

    aula 2.2

---
+++ src/pedido/pedido.entity.ts
@@ -0,0 +1,30 @@
+import {
+  Entity,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { StatusPedido } from './enum/statuspedido.enum';
+
+@Entity({ name: 'pedidos' })
+export class PedidoEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ name: 'valor_total', nullable: false })
+  valorTotal: number;
+
+  @Column({ name: 'status', enum: StatusPedido, nullable: false })
+  status: StatusPedido;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: string;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: string;
+
+  @DeleteDateColumn({ name: 'deleted_at' })
+  deletedAt: string;
+}
