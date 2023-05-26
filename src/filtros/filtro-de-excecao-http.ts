5098791078caaddc8d0d771632e86edc35030c55
Antônio Evaldo <antonio.evaldo349@gmail.com>
2023-05-26 16:15:47 -0300

    video-4.4

--- src/filtros/filtro-de-excecao-http.ts
+++ src/filtros/filtro-de-excecao-http.ts
@@ -3,19 +3,33 @@ import {
   Catch,
   ExceptionFilter,
   HttpException,
+  HttpStatus,
 } from '@nestjs/common';
-import { Response } from 'express';
+import { Request, Response } from 'express';
 
-@Catch(HttpException)
+@Catch()
 export class FiltroDeExcecaoHttp implements ExceptionFilter {
-  catch(excecao: HttpException, host: ArgumentsHost) {
+  catch(excecao: unknown, host: ArgumentsHost) {
     console.log(excecao);
 
     const contexto = host.switchToHttp();
     const resposta = contexto.getResponse<Response>();
+    const requisicao = contexto.getRequest<Request>();
 
-    const status = excecao.getStatus();
-    const body = excecao.getResponse();
+    const { status, body } =
+      excecao instanceof HttpException
+        ? {
+            status: excecao.getStatus(),
+            body: excecao.getResponse(),
+          }
+        : {
+            status: HttpStatus.INTERNAL_SERVER_ERROR,
+            body: {
+              statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+              timestamp: new Date().toISOString(),
+              path: requisicao.url,
+            },
+          };
 
     resposta.status(status).json(body);
   }
