4f5135dda95698498f0ebe2f6565f8dde6a7a0d7
Antônio Evaldo <antonio.evaldo349@gmail.com>
2023-05-26 17:05:56 -0300

    video-5.2

--- src/usuario/dto/AtualizaUsuario.dto.ts
+++ src/usuario/dto/AtualizaUsuario.dto.ts
@@ -1,17 +1,4 @@
-import { IsEmail, IsNotEmpty, IsOptional, MinLength } from 'class-validator';
-import { EmailEhUnico } from '../validacao/email-eh-unico.validator';
+import { PartialType } from '@nestjs/mapped-types';
+import { CriaUsuarioDTO } from './CriaUsuario.dto';
 
-export class AtualizaUsuarioDTO {
-  @IsNotEmpty({ message: 'O nome não pode ser vazio' })
-  @IsOptional()
-  nome: string;
-
-  @IsEmail(undefined, { message: 'O e-mail informado é inválido' })
-  @EmailEhUnico({ message: 'Já existe um usuário com este e-mail' })
-  @IsOptional()
-  email: string;
-
-  @MinLength(6, { message: 'A senha precisa ter pelo menos 6 caracteres' })
-  @IsOptional()
-  senha: string;
-}
+export class AtualizaUsuarioDTO extends PartialType(CriaUsuarioDTO) {}
