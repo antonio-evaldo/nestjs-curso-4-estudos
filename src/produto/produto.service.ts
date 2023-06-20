63fdb71f86ed9cfb75a3d06d6a39a0718e0dabfa
Antônio Evaldo <antonio.evaldo349@gmail.com>
2023-06-20 16:05:23 -0300

    projeto inicial

---
+++ src/produto/produto.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ListaProdutoDTO } from './dto/ListaProduto.dto';
+import { ProdutoEntity } from './produto.entity';
+import { Repository } from 'typeorm';
+import { AtualizaProdutoDTO } from './dto/AtualizaProduto.dto';
+import { CriaProdutoDTO } from './dto/CriaProduto.dto';
+
+@Injectable()
+export class ProdutoService {
+  constructor(
+    @InjectRepository(ProdutoEntity)
+    private readonly produtoRepository: Repository<ProdutoEntity>,
+  ) {}
+
+  async criaProduto(dadosProduto: CriaProdutoDTO) {
+    const produtoEntity = new ProdutoEntity();
+
+    produtoEntity.nome = dadosProduto.nome;
+    produtoEntity.valor = dadosProduto.valor;
+    produtoEntity.quantidade = dadosProduto.quantidade;
+    produtoEntity.descricao = dadosProduto.descricao;
+    produtoEntity.categoria = dadosProduto.categoria;
+    produtoEntity.caracteristicas = dadosProduto.caracteristicas;
+    produtoEntity.imagens = dadosProduto.imagens;
+
+    return await this.produtoRepository.save(produtoEntity);
+  }
+
+  async listProdutos() {
+    const produtosSalvos = await this.produtoRepository.find({
+      relations: {
+        imagens: true,
+        caracteristicas: true,
+      },
+    });
+    const produtosLista = produtosSalvos.map(
+      (produto) =>
+        new ListaProdutoDTO(
+          produto.id,
+          produto.nome,
+          produto.caracteristicas,
+          produto.imagens,
+        ),
+    );
+    return produtosLista;
+  }
+
+  async atualizaProduto(id: string, novosDados: AtualizaProdutoDTO) {
+    const entityName = await this.produtoRepository.findOneBy({ id });
+    Object.assign(entityName, novosDados);
+    await this.produtoRepository.save(entityName);
+  }
+
+  async deletaProduto(id: string) {
+    await this.produtoRepository.delete(id);
+  }
+}
