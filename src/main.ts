d88b946ccfb449bb798ba574598fdd059f6fa209
camilapessoa <camilabentespessoa@gmail.com>
2023-07-11 14:45:53 -0300

    Exemplo básico de Logger Co-authored-by: Laranja  <emersonlaranja@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,17 @@ import { useContainer } from 'class-validator';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    // logger:false
+   logger: ['error']
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
       whitelist: true,
       forbidNonWhitelisted: true,
     }),
+    
   );
 
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
